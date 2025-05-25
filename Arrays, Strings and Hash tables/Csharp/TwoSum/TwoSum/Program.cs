ae92ea45ebe7206fe7893cc97ec048806a04c53b
GAA6GA <AndonyAbdala.GarciaGarcia@mx.bosch.com>
2025-05-25 10:34:15 -0600

    Function working in C# as well

---
+++ Arrays, Strings and Hash tables/Csharp/TwoSum/TwoSum/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TwoSum;
+
+class TwoSum()
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 5, 8 };
+        int targetValue = 4;
+        int[] result = TwoSumMethod.TwoSum(array, targetValue);
+
+        string resultString = $"The index to sum {targetValue} are: ";
+        int i = 0 ;
+        foreach (int resultItem in result)
+        {
+            if (i == 1)
+                resultString += resultItem.ToString();
+            else
+                resultString += resultItem.ToString() + ", ";
+            i++;
+        }
+        Console.WriteLine(resultString);
+    }
+}
---
+++ Arrays, Strings and Hash tables/Csharp/TwoSum/TwoSum/TwoSumMethod.cs
@@ -0,0 +1,43 @@
+﻿//# /*
+//#  * Dado un array de números enteros y un target, retorna los índices de dos
+//#  * números para los que la suma de ambos sea igual al target.
+//#  *
+//#  * Puedes asumir que hay solamente una solución.
+//#  *
+//#  * Ejemplo 1:
+//#  *  Input: nums = [9,2,5,6], target = 7
+//#  *  Output: [1,2]
+//#  *  Explicación: nums[1] + nums[2] == 7, devolvemos [1, 2].
+//#  *
+//#  * Ejemplo 2:
+//#  *  Input: nums = [9,2,5,6], target = 100
+//#  *  Output: null
+//#  */
+
+namespace TwoSum
+{
+    class TwoSumMethod
+    {
+        static public int[] TwoSum(int[] nums, int targetValue)
+        { 
+            if (nums == null || nums.Length < 2) return new int[] { };
+
+            Dictionary<int, int> complements = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (complements.ContainsKey(nums[i]))
+                {
+                    return new int[] { complements[nums[i]], i };
+                }
+
+                int complement = targetValue - nums[i];
+
+                complements.Add(complement, i);
+            }
+
+            int[] result = new int[2];
+            return result;
+        }
+    }
+}
